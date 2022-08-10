d4ae5fff3849805291224165e7b5db34f7a25075
Diana Murtazina <murka8831@yandex.ru>
2022-08-10 16:23:21 +0400

    a request has been written and a response has been received for the ekaterinburg-rasnodar flight on ugust 12, 2022

--- script.js
+++ script.js
@@ -9,9 +9,35 @@ const citiesApi = "dataBase/cities.json";
 // const citiesApi = "http://api.travelpayouts.com/data/ru/cities.json";
 const proxy = "https://cors-anywhere.herokuapp.com/";
 
+const API_KEY = "64419c0306fba50f4b589a9f96cfcc8a";
+
+const calendar = new URL(
+    "https://api.travelpayouts.com/aviasales/v3/grouped_prices"
+);
+calendar.searchParams.set("origin", "SVX");
+calendar.searchParams.set("destination", "KRR");
+calendar.searchParams.set("departure_at", "2022-08-12");
+calendar.searchParams.set("token", API_KEY);
+
 let city = [];
 
 const getData = (url, callBack) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+
+    request.addEventListener("readystatechange", () => {
+        if (request.readyState !== 4) return;
+
+        if (request.status === 200) {
+            callBack(request.response);
+        } else {
+            console.error(request.status);
+        }
+    });
+    request.send();
+};
+
+const getTicket = (url, callBack) => {
     const request = new XMLHttpRequest();
     request.open("GET", url);
 
@@ -69,4 +95,8 @@ dropdownCitiesTo.addEventListener("click", () =>
 
 getData(citiesApi, (data) => {
     city = JSON.parse(data).filter((item) => item.name);
+});
+
+getTicket(proxy + calendar, (data) => {
+    console.log(JSON.parse(data));
 });
